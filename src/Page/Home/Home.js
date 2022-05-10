e4f281f22e7c0d0f01abe566339949269d3004cf
Shakil <shakil11271@gmail.com>
2022-05-11 00:59:39 +0600

    add appoinment section

--- src/Page/Home/Home.js
+++ src/Page/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Appoinment from './Appoinment';
 import Banner from './Banner';
 import Info from './Info';
 import Services from './Services';
@@ -11,6 +12,7 @@ const Home = () => {
             <Info></Info>
             <Services></Services>
             <Treatment></Treatment>
+            <Appoinment></Appoinment>
         </div>
     );
 };

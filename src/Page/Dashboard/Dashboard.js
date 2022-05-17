c6b8246b97d814fdc4aa0a4c96b81d039d18bf7c
Shakil <shakil11271@gmail.com>
2022-05-17 20:33:06 +0600

    secure admin

--- src/Page/Dashboard/Dashboard.js
+++ src/Page/Dashboard/Dashboard.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth'
+import auth from '../../firebase.init'
+import useAdmin from '../../hook/useAdmin';
 
 const Dashboard = () => {
+    const [user] = useAuthState(auth)
+    const [admin] = useAdmin(user)
     return (
         <div class="drawer drawer-mobile">
             <input id="sidebar" type="checkbox" class="drawer-toggle" />
@@ -18,7 +23,7 @@ const Dashboard = () => {
                     {/* <!-- Sidebar content here --> */}
                     <li><Link to='/dashboard'>My Appointment</Link></li>
                     <li><Link to='/dashboard/reviews'>My Reviews</Link></li>
-                    <li><Link to='/dashboard/users'>Users</Link></li>
+                    <li>{admin && <Link to='/dashboard/users'>Users</Link>}</li>
                 </ul>
 
             </div>
--- src/Page/Dashboard/Users.js
+++ src/Page/Dashboard/Users.js
@@ -1,9 +1,67 @@
+import { data } from 'autoprefixer';
 import React from 'react';
+import { useQuery } from 'react-query';
+import { toast } from 'react-toastify';
+import Loading from '../Shared/Loading/Loading'
 
 const Users = () => {
+    const { data: users, isLoading, refetch } = useQuery('users', () => fetch('http://localhost:5000/user', {
+        method: 'GET',
+        headers: {
+            'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    })
+        .then(res => res.json()))
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+
+    const makeAdmin = (email) => {
+        fetch(`http://localhost:5000/user/admin/${email}`, {
+            method: 'PUT',
+            headers: {
+                'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => {
+                if (res.status === 403) {
+                    return toast.error('You do not have permission to make an admin')
+                }
+                return res.json()
+            })
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    refetch()
+                    toast.success('Successfully Make an Admin')
+                }
+            })
+    }
     return (
         <div>
-            <div className="text-2xl">This is All Users</div>
+            <div className="text-2xl">This is All Users : {users.length}</div>
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Email</th>
+                            <th>Access</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user, index) =>
+                                <tr key={user._id}>
+                                    <th>{index + 1}</th>
+                                    <td>{user.email}</td>
+                                    <td>{user.role !== 'admin' && <button onClick={() => makeAdmin(user.email)} class="btn btn-xs">Make Admin</button>}</td>
+                                    <td><button class="btn btn-xs">Remove User</button></td>
+                                </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
